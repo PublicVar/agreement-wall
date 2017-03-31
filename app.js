d49779f895a076d4de15d43a743ac486c5da3083
PublicVar <dev.publicvar@gmail.com>
2017-03-31 14:58:22 +0400

    Remove commented code

--- app.js
+++ app.js
@@ -15,34 +15,7 @@ var agreement = require('./routes/agreement');
 
 
 var app = express();
-//Add winston log to a file and disable the default console outpu
-// const logger = new (winston.Logger)({
-//     transports: [
-//       //log : error, warn, info
-//       new winston.transports.File({ 
-//         level: 'info',
-//         filename: './logs/all.log' ,
-//         json: true,
-//         maxsize: 5242880, //5MB
-//         maxFiles: 5,
-//         colorize: false
-//       }),
-//       //log debug
-//       new winston.transports.Console({
-//             level: 'debug',
-//             handleExceptions: true,
-//             json: false,
-//             colorize: true,
-//             filename: './logs/debug.log',
-//         })
-//     ],
-//     exitOnError: false
-// });
-// logger.stream = {
-//     write: function(message, encoding){
-//         logger.info(message);
-//     }
-// };
+
 
 //make morgan logger for http request logs into file throught winston
 app.use(morgan("combined", { "stream": logger.stream }));
